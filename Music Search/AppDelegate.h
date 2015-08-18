bcf90bf849a0660124dc7ce79279849f499aec59
erickookie <yue.eyqt@gmail.com>
2015-08-18 18:10:19 -0400

    Initial Commit

---
+++ Music Search/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Music Search
+//
+//  Created by MCS on 8/18/15.
+//  Copyright (c) 2015 MCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
